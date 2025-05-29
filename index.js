d71b1dba0dd66503ff505d6c91733fca09032483
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-29 16:35:10 +0200

    Added Render in JS + updated HTML

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const color = document.getElementById("color");
 const schemeMode = document.getElementById("scheme-mode");
 const btn = document.getElementById("btn");
+const colorScheme = document.getElementById("color-scheme");
+let colorSchemeArr = [];
+
+function renderColorScheme() {
+  let colorSchemeHtml = "";
+
+  colorSchemeArr.forEach((color) => {
+    console.log(color);
+    colorSchemeHtml += `
+    <div class="color-block">
+        <img
+          src="https://www.thecolorapi.com/id?format=svg&named=false&hex=${color.hex.clean}"
+          alt="${color.name.value}"
+        />
+      </div>
+`;
+  });
+
+  colorScheme.innerHTML = colorSchemeHtml;
+}
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   const chosenColor = color.value.substring(1);
   const chosenSchemeMode = schemeMode.value;
-  console.log(chosenColor, chosenSchemeMode);
 
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenSchemeMode}`
   )
     .then((res) => res.json())
-    .then((data) => console.log(data.colors));
+    .then((data) => {
+      colorSchemeArr = data.colors;
+      renderColorScheme(colorSchemeArr);
+    });
 });
