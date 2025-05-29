e135e340dd8d4cb7e8b4a20e970c1d4b7e9c151b
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-29 15:26:51 +0200

    Updated CSS + added JS btn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic")
-  .then((res) => res.json())
-  .then((data) => console.log(data.colors[0].hex));
+const color = document.getElementById("color");
+const schemeMode = document.getElementById("scheme-mode");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const chosenColor = color.value.substring(1);
+  const chosenSchemeMode = schemeMode.value;
+  console.log(chosenColor, chosenSchemeMode);
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenSchemeMode}`
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data.colors));
+});
