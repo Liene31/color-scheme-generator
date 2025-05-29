1218614640021615ee8fa5e3f423a56f92bf674f
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-29 17:48:22 +0200

    Refactored the code

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 let colorSchemeArr = [];
 
+// Select the color and mode & returns the values
+
+document.getElementById("btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const selectedColor = document.getElementById("color").value.substring(1);
+  const sekectedSchemeMode = document.getElementById("scheme-mode").value;
+
+  fetchColors(selectedColor, sekectedSchemeMode);
+});
+
+// Fetch the color scheme from API & retunrs the Array
+
+function fetchColors(color, mode) {
+  fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
+    .then((res) => res.json())
+    .then((data) => {
+      colorSchemeArr = data.colors;
+      renderColorScheme(colorSchemeArr);
+    });
+}
+
+// Renders the color scheme in HTML
+
 function renderColorScheme(array) {
   console.log(array);
   let colorSchemeHtml = "";
@@ -18,19 +41,10 @@ function renderColorScheme(array) {
   document.getElementById("color-scheme").innerHTML = colorSchemeHtml;
 }
 
-document.getElementById("btn").addEventListener("click", function (e) {
-  e.preventDefault();
-  const chosenColor = document.getElementById("color").value.substring(1);
-  const chosenSchemeMode = document.getElementById("scheme-mode").value;
+// Render default colors on initial load
 
-  fetch(
-    `https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenSchemeMode}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      colorSchemeArr = data.colors;
-      renderColorScheme(colorSchemeArr);
-    });
+window.addEventListener("DOMContentLoaded", function () {
+  const defaultHex = "ff0000";
+  const defaultMode = "monochrome";
+  fetchColors(defaultHex, defaultMode);
 });
-
-renderColorScheme(colorSchemeArr);
